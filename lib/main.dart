4d24936b33931c235502fa90f1a98fcb08fca83b
CyberDaco <damiravdic9@gmail.com>
2020-01-03 12:38:42 +1100

    added Question class as well as the question list implementation in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'question.dart';
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -25,13 +25,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-
+  
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+  List<Question> questions = [
+    Question(question: 'You can lead a cow down stairs but not up stairs.', answer: false),
+    Question(question: 'Approximately one quarter of human bones are in the feet.', answer: true),
+    Question(question: 'A slug\'s blood is green.', answer: true)
   ];
+
   List<bool> answers = [false,true,true];
 
   int questionNumber = 0;
@@ -48,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questions[questionNumber].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,9 +74,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumber];
-
-                if(correctAnswer == true){
+                if(questions[questionNumber].answer == true){
                   //correct answer
                 }
                 else{
@@ -100,9 +100,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumber];
-
-                if(correctAnswer == false){
+                if(questions[questionNumber].answer == false){
                   //correct answer
                 }
                 else{
@@ -122,19 +120,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-class Question {
-  String question;
-  bool answer;
-
-  Question({String question, bool answer}){
-    this.question = question;
-    this.answer = answer;
-  }
-}
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
